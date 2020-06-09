f7dda7f8f2db382f2a809e3baa3566f474a59835
AngelXool <martin.rios.yam@gmail.com>
2020-06-09 15:42:59 -0500

    First Commit

---
+++ EnergyController.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EnergyController.Models
+{
+    public class Class1
+    {
+    }
+}
---
+++ EnergyController.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EnergyController.Services
+{
+    public class Class1
+    {
+    }
+}
